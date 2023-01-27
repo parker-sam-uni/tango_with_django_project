3f0954956c19ae8c703537725084acf38656fdfb
Sam Parker <2620865P@student.gla.ac.uk>
2023-01-27 09:50:24 +0000

    Chapter 7 complete and working correctly

--- rango/models.py
+++ rango/models.py
@@ -20,7 +20,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField(default = '')
     views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.title
 
